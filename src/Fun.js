9404c461bcbb46af8ded3723b7bfa65a623fb060
Ashutosh <Axhutoxh@github.com>
2020-11-03 19:36:38 +0530

    2ndcommit

--- src/Fun.js
+++ src/Fun.js
@@ -1,59 +1,81 @@
 import { render } from "@testing-library/react";
-import React from "react";
+import React,{ useState , useEffect ,useRef} from "react";
 import "./assets/css/style.css";
 import Images from "./components/Images";
 
-// function Fun({title}){
-//     return <div>
-//         <div className = "box">
-//         Hello Dude {title}
 
-//         </div>
-//      </div>
-// }
-class Fun extends React.Component{
-    constructor(props){
-        console.log("Fun constructor");
-        super(props);
-        this.state ={title:"hello dude ", isShowing:false};
+function Fun(){
+    const [title, setTitle] = useState("hello Dude"); 
+    const [isShowing, setisShowing] = useState(false);
+    const [didMount, setdidMount] = useState(false);
+    const mountRef = useRef(false);
+   
+//component did mount only   
+    useEffect(()=> {
+        setdidMount(true);
+        console.log("Fun Mount");
+    },[]);
+
+//component will updated
+    useEffect(()=> {
+        if (mountRef.current){
+        console.log("fun updated");
+        }
+        else{
+            mountRef.current=true;
+        }
+    },[isShowing]);
+
+
+    function toggleButton(){
+        setisShowing(!isShowing);
     }
-// stattes are Immutable 
-
-componentDidMount(){
-    console.log("Fun Mount");
-    //this.setState({title:"Hello React"});
-}
-
-componentDidUpdate(){
-    console.log("Fun Updated")
-}
-
-
-toggleButton = ()=>{
-    (this.setState({isShowing:!this.state.isShowing}))
-}
-
-
-
-render(){
-    console.log("Fun render");
+    
     return (
-        <section className = "flex justify-center">
-            <div className = "w1/2">
-                <div className = "my-5 text-center">
-                    {this.state.title}
-                <div className="text-center" > 
-                    <button className="p-1 bg-blue-500 text-white my-2"
-                     onClick ={this.toggleButton} >
-                        Toggle Image
-                    </button>
-                </div>
-                {this.state.isShowing ? <Images /> :null}
-                 </div>
-
+         <section className = "flex justify-center">
+            <div className = "w-1/2">
+                 <div className = "my-5 text-center"> 
+                    {title}
+                    </div>
+                    <div className="text-center" > 
+                        <button className="p-1 bg-blue-500 text-white my-2" onClick ={toggleButton} >
+                            Toggle Image
+                        </button>
+                        {isShowing ? <Images /> :null}
+                 </div> 
             </div>
         </section>
+
     );
 }
-}
+
+// class Fun extends React.Component{
+//     // constructor(props){
+//     //     console.log("Fun constructor");
+//     //     super(props);
+//     //     .state ={title:"hello dude ", isShowing:false};
+//     // }
+// // stattes are Immutable 
+
+// componentDidMount(){
+//     console.log("Fun Mount");
+//    // .setState({title:"Hello React"});
+// }
+
+// componentDidUpdate(){
+//     console.log("Fun Updated")
+// }
+
+
+
+
+
+// render(){
+//     console.log("Fun render");
+//     return (
+
+//     );
+// }
+// }
+
 export default Fun;
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,29 +1,84 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default class Images extends Component {
 
-    constructor(props){
+export default function Images() {
 
-        super(props);
-            this.state ={interval : null};
-        
+   // const [myinterval, setmyinterval] = useState(null);
+
+    useEffect(()=> {
+     console.log("images mounted");
+           const interval = setInterval(()=>{
+             console.log("hello");
+             },1000)
+         ;
+
+        return()=>{
+            clearInterval(interval);
         }
 
-    componentDidMount(){
-        this.setState({
-            interval : setInterval(()=>{
-                console.log("1");
-            },2000),
-        });
-    }
 
-    componentWillUnmount(){
-        console.log("Images Unmount");
-        clearInterval(this.state.interval);
-    }
-    render() {
+    },[])
+    const Images = [ 
+    "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1604367078108-3ed095682132?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1604361709763-44f7fc6dd075?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1602526428496-8346b5cf9954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=727&q=80",
+    "https://images.unsplash.com/photo-1604329051903-d89ddd523330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
+    "https://images.unsplash.com/photo-1604326208115-ce65e78475c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=799&q=80",
+    "https://images.unsplash.com/photo-1604102387673-48ab602201ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+    ];
+
+function ShowImage(prams){
+    return (Images.map((image)=>{
         return (
-            <img src="https://images.unsplash.com/photo-1604102387673-48ab602201ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"/>
-        )
-    }
+    <div>
+        <img src={image} width ="180" />
+    </div>
+    );
+  })
+    );}
+
+    return (
+        <section>
+            <div className="flex justify-center"> 
+                <ShowImage  />
+            </div>
+                <div className="flex justify-between my-5"> 
+                    <input type="text" className="p-2 border border-gray-800 shadow "/>
+                    <button className="p-1 bg-green-600 text-white">Add New</button>
+                </div>
+
+
+
+        </section>
+    );
 }
+
+
+ {/* export default class Images extends Component { 
+
+//     constructor(props){
+
+//         super(props);
+//             this.state ={interval : null};
+        
+//         }
+
+//     componentDidMount(){
+//         console.log("Images mount");
+//         this.setState({
+//             interval : setInterval(()=>{
+//                 console.log("1");
+//             },1000),
+//         });
+//     }
+
+//     componentWillUnmount(){
+//         console.log("Images Unmount");
+//         clearInterval(this.state.interval);
+//     }
+//     render() {
+//         return (
+//         )
+//     }
+// } */}

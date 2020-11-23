cb62e5130cb18488dc045d1af508dd6a5332181f
Axhutoxh <ashutosh.chandra.520@gmail.com>
2020-11-24 03:04:58 +0530

    12th commit

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,16 +1,27 @@
-import { useFormik } from 'formik';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
 import React from 'react';
 import * as Yup from 'yup';
+import firebase from 'firebase'
+import { useHistory } from 'react-router-dom';
 
 export default function SignUp(){
 
-    const formik =useFormik({
-        initialValues:{email:'' ,password:''},
-        onSubmit:(value) =>{
-            console.log("Formik",value);
-        },
-        validationSchema:Yup.object(
-            {
+    // if(isLoggedIn) return <Redirect to="/" />
+    const history = useHistory();
+    return (
+        <Formik
+        
+        initialValues={{email:'' ,password:''}}
+        onSubmit={(value,formikBag) =>{
+          firebase.auth().createUserWithEmailAndPassword(value.email,value.password)
+          .then((res)=> {
+              history.replace('/');
+
+          }).catch((e)=> {
+              formikBag.setFieldError('email',e.message);
+          })
+        }}
+        validationSchema={Yup.object({
                 email:Yup.string()
                 .required('Email is required')
                 .email(),
@@ -20,60 +31,50 @@ export default function SignUp(){
                 .required('Password is required')
                 .min(6)
                 
-            }
-        ),
-    })
+            })}
+            >
+                        <div className="flex h-screen bg-gray-200">
+                        <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-purple-900 to-purple-700">
+                
+                           <Form className="m-5 w-10/12" >
+                           <h1 className="w-full text-3xl tracking-widest text-center my-6">
+                               SignUp Here
+                               </h1>
+                               <div className="w-full my-6">
+                               <Field 
+                               name= "email"
+                               type="email" 
+                               className="p-2 rounded shadow w-full text-black"
+                               placeholder="Email or Username"                               
+                               />
+                                 <ErrorMessage name="email" />
 
-    // if(isLoggedIn) return <Redirect to="/" />
+                            </div>
+                            <div className="w-full my-6">
+                            <Field 
+                               name= "password"
+                               type="password" 
+                               className="p-2 rounded shadow w-full text-black"
+                               placeholder="Email or Username"                               
+                               />
+                                
+                                <ErrorMessage name="password" />
 
-    return (<div className="flex h-screen bg-gray-200">
-        <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-purple-900 to-purple-700">
-
-           <form className="m-5 w-10/12" onSubmit={formik.handleSubmit} >
-           <h1 className="w-full text-3xl tracking-widest text-center my-6">
-               SignUp Here
-               </h1>
-               <div className="w-full my-6">
-                <input
-                 type="email" 
-                 className="p-2 rounded shadow w-full text-black"
-                 placeholder="Email or Username"
-                 name="email"
-                 value={formik.values.email}
-                 onChange={formik.handleChange}
-                 onBlur={formik.handleBlur}
-                 />
-                 {formik.touched.email && formik.errors.email ?(
-                 <p>{formik.errors.email}</p>):null
-                    }
-            </div>
-            <div className="w-full my-6">
-                <input
-                 type="password" 
-                 className="p-2 rounded shadow w-full text-black"
-                 placeholder="Password"
-                 name="password"
-                 value={formik.values.password}
-                 onChange={formik.handleChange}
-                 onBlur={formik.handleBlur}
-                 />
-                 {formik.touched.password && formik.errors.password ?(
-                 <p>{formik.errors.password}</p>):null
-                    }
-            </div>
-
-            <div className="w-full my-10">
-                <button
-                 type="submit"
-                 className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-300 text-black"
-                 >
-                     {/* {
-                         isLoading ? <i className="fas fa-circle-notch fa-spin "></i>:"Login"
-                     } */}
-                     Sign Up
-                </button>
-            </div>
-           </form>
-        </div>
-    </div>
+                            </div>
+                
+                            <div className="w-full my-10">
+                                <button
+                                 type="submit"
+                                 className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-300 text-black"
+                                 >
+                                     {/* {
+                                         isLoading ? <i className="fas fa-circle-notch fa-spin "></i>:"Login"
+                                     } */}
+                                     Sign Up
+                                </button>
+                            </div>
+                           </Form>
+                        </div>
+                    </div>
+    </Formik>
     )}
